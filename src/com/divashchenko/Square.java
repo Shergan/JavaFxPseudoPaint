8edf294e4ea4beb90c661cc0005c2289ead3073f
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-03-07 14:50:38 +0200

    add Figure->Square

---
+++ src/com/divashchenko/Square.java
@@ -0,0 +1,26 @@
+package com.divashchenko;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class Square extends Figure {
+
+    Square(GraphicsContext gc, double x, double y, List<Shape> shapes) {
+        super(gc, x, y, shapes);
+    }
+
+    @Override
+    public void draw() {
+        diameter = 30;
+
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+
+        gc.fillRect(x, y, diameter, diameter);
+        gc.strokeRect(x, y, diameter, diameter);
+    }
+
+}
