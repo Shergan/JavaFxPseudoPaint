6010b7ce48e8e67d163d86939cd4f856a917b873
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-03-07 14:50:56 +0200

    add Board class

---
+++ src/com/divashchenko/Board.java
@@ -0,0 +1,52 @@
+package com.divashchenko;
+
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    public enum Figures {
+        BALL, SQUARE, TRIANLGE
+    }
+
+    private GraphicsContext gc;
+
+    private List<Shape> shapes = new ArrayList<>();
+
+    public Board(GraphicsContext gc) {
+        this.gc = gc;
+    }
+
+    public void move() {
+        for (Shape shape : shapes) {
+            shape.move();
+        }
+    }
+
+    public void draw() {
+        clean();
+        for (Shape shape : shapes) {
+            shape.draw();
+        }
+    }
+
+    public void addFigure(Figures figures) {
+        switch (figures) {
+            case BALL:
+                shapes.add(new Ball(gc, 10, 10, shapes));
+                break;
+            case SQUARE:
+                shapes.add(new Square(gc, 10, 10, shapes));
+                break;
+            case TRIANLGE:
+                break;
+        }
+    }
+
+    private void clean() {
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+    }
+
+}
