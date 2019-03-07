82222f24f1df5a507d2f87a72d4ccc1031446773
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-03-07 14:50:20 +0200

    add Figure->Ball

---
+++ src/com/divashchenko/Ball.java
@@ -0,0 +1,26 @@
+package com.divashchenko;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class Ball extends Figure {
+
+    Ball(GraphicsContext gc, double x, double y, List<Shape> shapes) {
+        super(gc, x, y, shapes);
+    }
+
+    @Override
+    public void draw() {
+        diameter = 30;
+
+        gc.setFill(Color.RED);
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(2);
+
+        gc.fillOval(x, y, diameter, diameter);
+        gc.strokeOval(x, y, diameter, diameter);
+    }
+
+}
