c229d3be68c203f5a6aab7d8c5928ba82986bc76
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-03-07 14:49:50 +0200

    add abstract class

---
+++ src/com/divashchenko/Figure.java
@@ -0,0 +1,30 @@
+package com.divashchenko;
+
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.List;
+
+public abstract class Figure implements Shape {
+
+    protected double diameter;
+
+    protected GraphicsContext gc;
+    protected double x;
+    protected double y;
+    protected List<Shape> shapes;
+
+    protected Figure(GraphicsContext gc, double x, double y, List<Shape> shapes) {
+        this.gc = gc;
+        this.x = x;
+        this.y = y;
+        this.shapes = shapes;
+    }
+
+    @Override
+    public void move() {
+        //TODO move
+    }
+
+    @Override
+    public abstract void draw();
+}
